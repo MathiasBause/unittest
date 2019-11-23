ce92b83b79d18c3ab32f8716d0716c3876366ae6
Mathias <gmathiasbll@gmail.com>
2019-11-23 10:16:22 -0400

    Ahora si

--- SI220Z1/testing/UnitTest1.cs
+++ SI220Z1/testing/UnitTest1.cs
@@ -10,10 +10,34 @@ namespace testing
         public void TestSaludar()
         {
             //Organizar
-
+            SI220Z1.Saludar obj = new SI220Z1.Saludar();
+            String valoresperado = "HOLA MUNDO";
             //Actuar
-            
+            string valoresultado = obj.DecirHola();
             //Afirmar
+            Assert.AreEqual(valoresperado, valoresultado);
+        }
+        [TestMethod]
+        public void TestSuma2Mas2Iguala4()
+        {
+            //Organizar
+            SI220Z1.Aritmetica obj = new SI220Z1.Aritmetica();
+            int valoresperado = 4;
+            //Actuar
+            int valoresultado = obj.sumar(2,2);
+            //Afirmar
+            Assert.AreEqual(valoresperado, valoresultado);
+        }
+        [TestMethod]
+        public void TestSuma2MasNullIguala2()
+        {
+            //Organizar
+            SI220Z1.Aritmetica obj = new SI220Z1.Aritmetica();
+            int valoresperado = 2;
+            //Actuar
+            int valoresultado = obj.sumar(2, null);
+            //Afirmar
+            Assert.AreEqual(valoresperado, valoresultado);
         }
     }
 }
